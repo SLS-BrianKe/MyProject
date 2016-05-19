63451af19c2c1ed2399c530bbf0093e8bc5a1933
Brian Ke <brian.ke@silverlining-systems.com>
2016-05-19 17:42:31 +0800

    initial commit

---
+++ getid_tool/setup.py
@@ -0,0 +1,16 @@
+#from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name = 'getid_tools',
+    packages = ['getid_tool'],
+    scripts = ['getid_tool/getid'],
+    version = '1.2',
+    description = 'SLS Get ID Tool',
+    author = 'sls.jarvis, SLS-BrianKe',
+    author_email = 'pkg-admin@silverlining-systems.com',
+    url = 'https://github.com/SilverLiningSystems/testbench',
+    download_url = 'https://github.com/SilverLiningSystems/testbench/archive/v1.1.tar.gz',
+    keywords = ['Calxeda SilverLining SLS'],
+    classifiers = ['License :: OSI Approved :: BSD License', 'Programming Language :: Python :: 2.7'],
+)
